f794462a3f7d73611053f2940d956f344376c12a
Shine <stbnlen@gmail.com>
2022-03-09 20:18:37 -0300

    [ADD] manifest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/* eslint-disable prettier/prettier */
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
   },
-};
+});
